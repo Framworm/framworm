b972bccfe59038d42fb5957a4f990991e8becc49
aiglematth <aiglematth@protonmail.com>
2021-05-05 08:58:34 +0200

    Ajout boucle

---
+++ framworm/events.py
@@ -0,0 +1,18 @@
+#Imports
+from queue import Queue as FIFO
+
+#Classes
+class Events():
+    """
+    Classe repreésentant notre boucle d'éxecution
+    """
+    def __init__(self):
+        """
+        Constructeur de la classe
+        """
+        pass
+
+    def run(self):
+        """
+        Méthode principale, permet de lancer la boucle pseudo infinie qui gère notre vers
+        """
