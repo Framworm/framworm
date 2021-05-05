3c095465af1c67efe1288df686241e9392c24e23
aiglematth <aiglematth@protonmail.com>
2021-05-05 11:06:35 +0200

    test

--- framworm/__init__.py
+++ framworm/__init__.py
@@ -1,4 +1,4 @@
-from os.path import dirname, basename, isfile, join
-import glob
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+import pkgutil
+__path__ = pkgutil.extend_path(__path__, __name__)
+for imp, module, ispackage in pkgutil.walk_packages(path=__path__, prefix=__name__+'.'):
+  __import__(module)
---
+++ framworm/deletePycache.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+find . -name __pycache__ -type f -exec rm -Rf {} \;
