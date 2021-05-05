085120a01acb48528c98a4d78e9935cfe17970d2
aiglematth <aiglematth@protonmail.com>
2021-05-05 11:23:49 +0200

    add requirements

--- framworm/__init__.py
+++ framworm/__init__.py
@@ -1,4 +1,13 @@
 import pkgutil
 __path__ = pkgutil.extend_path(__path__, __name__)
+__all__  = []
 for imp, module, ispackage in pkgutil.walk_packages(path=__path__, prefix=__name__+'.'):
   __import__(module)
+  if "__init__" not in module and "abstract" not in module:
+    __all__.append(module)
+
+del pkgutil
+try:
+  del module, imp, ispackage
+except:
+  pass
--- framworm/attaques/__init__.py
+++ framworm/attaques/__init__.py
@@ -1,4 +1,13 @@
 import pkgutil
 __path__ = pkgutil.extend_path(__path__, __name__)
+__all__  = []
 for imp, module, ispackage in pkgutil.walk_packages(path=__path__, prefix=__name__+'.'):
   __import__(module)
+  if module != "__init__.py" and module != "abstract.py":
+    __all__.append(module)
+
+del pkgutil
+try:
+  del module, imp, ispackage
+except:
+  pass
